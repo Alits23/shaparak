ab9c25d3f5424270a823f0bbb15962cf07c0fde7
Ali Tashakori <ali.tashakori.23@gmail.com>
2024-01-29 03:05:58 +0330

    created profile screen

---
+++ lib/view/profile_screen.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import '../constans/color.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: CustomColors.backgroundScreenColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            AppBarProfile(),
+            InfoUser(),
+            Wrap(
+              spacing: 20.0,
+              runSpacing: 20.0,
+              alignment: WrapAlignment.end,
+              children: [
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+                ItemList(),
+              ],
+            ),
+            Spacer(),
+            Text(
+              'Shaparak Shop',
+              style: TextStyle(
+                fontFamily: 'sm',
+                fontSize: 12.0,
+                color: CustomColors.gery,
+              ),
+            ),
+            Text(
+              'v -2.2.04',
+              style: TextStyle(
+                fontFamily: 'sm',
+                fontSize: 12.0,
+                color: CustomColors.gery,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  const ItemList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 56.0,
+          height: 56.0,
+          decoration: BoxDecoration(
+            color: CustomColors.blueIndicator,
+            borderRadius: BorderRadius.circular(15.0),
+            boxShadow: const [
+              BoxShadow(
+                blurRadius: 25.0,
+                color: CustomColors.blueIndicator,
+                spreadRadius: -12.0,
+                offset: Offset(0.0, 15.0),
+              ),
+            ],
+          ),
+          child: const Icon(
+            Icons.settings,
+            color: CustomColors.white,
+            size: 35.0,
+          ),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        const Text(
+          'تنظیمات',
+          style: TextStyle(
+            fontFamily: 'sb',
+            fontSize: 14,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class InfoUser extends StatelessWidget {
+  const InfoUser({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+          'علی تشکری صباغ',
+          style: TextStyle(
+            fontFamily: 'sb',
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          '# 99154028',
+          style: TextStyle(
+            fontFamily: 'sm',
+            fontSize: 14,
+            color: CustomColors.gery,
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    );
+  }
+}
+
+class AppBarProfile extends StatelessWidget {
+  const AppBarProfile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+          right: 44.0, left: 44.0, bottom: 32.0, top: 5.0),
+      child: Container(
+        height: 56.0,
+        decoration: BoxDecoration(
+          color: CustomColors.white,
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Row(
+          children: [
+            const SizedBox(
+              width: 16.0,
+            ),
+            Image.asset('assets/images/icon_apple_blue.png'),
+            const Expanded(
+              child: Text(
+                'حساب کاربری',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'sb',
+                  color: CustomColors.blue,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 38.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -5,6 +5,7 @@ import 'package:shaparak/constans/color.dart';
 import 'package:shaparak/view/category_screen.dart';
 import 'package:shaparak/view/home_screen.dart';
 import 'package:shaparak/view/product_list_screen.dart';
+import 'package:shaparak/view/profile_screen.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({super.key});
@@ -14,16 +15,17 @@ class BottomNavigation extends StatefulWidget {
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
-  int selectedIndex = 1;
+  int selectedIndex = 3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(
         index: selectedIndex,
         children: const [
+          ProfileScreen(),
           Categoryscreen(),
-          HomeScreen(),
           ProductListScreen(),
+          HomeScreen(),
         ],
       ),
       bottomNavigationBar: ClipRRect(
@@ -36,7 +38,7 @@ class _BottomNavigationState extends State<BottomNavigation> {
             elevation: 0.0,
             selectedLabelStyle: const TextStyle(
               fontFamily: 'sb',
-              fontSize: 10.0,
+              fontSize: 12.0,
               color: CustomColors.blue,
             ),
             unselectedLabelStyle: const TextStyle(
@@ -50,6 +52,25 @@ class _BottomNavigationState extends State<BottomNavigation> {
               });
             },
             items: [
+              // 0
+              BottomNavigationBarItem(
+                icon: Container(
+                  child: Image.asset('assets/images/icon_profile.png'),
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: CustomColors.blueIndicator,
+                        blurRadius: 20.0,
+                        spreadRadius: -7.0,
+                        offset: Offset(0.0, 13.0),
+                      ),
+                    ],
+                  ),
+                ),
+                activeIcon:
+                    Image.asset('assets/images/icon_profile_active.png'),
+                label: 'پروفایل',
+              ),
               // 1
               BottomNavigationBarItem(
                 icon: Container(
@@ -72,24 +93,6 @@ class _BottomNavigationState extends State<BottomNavigation> {
               // 2
               BottomNavigationBarItem(
                 icon: Container(
-                  child: Image.asset('assets/images/icon_home.png'),
-                  decoration: const BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: CustomColors.blueIndicator,
-                        blurRadius: 20.0,
-                        spreadRadius: -7.0,
-                        offset: Offset(0.0, 13.0),
-                      ),
-                    ],
-                  ),
-                ),
-                activeIcon: Image.asset('assets/images/icon_home_active.png'),
-                label: 'صفحه اصلی',
-              ),
-              // 3
-              BottomNavigationBarItem(
-                icon: Container(
                   child: Image.asset('assets/images/icon_basket.png'),
                   decoration: const BoxDecoration(
                     boxShadow: [
@@ -104,6 +107,24 @@ class _BottomNavigationState extends State<BottomNavigation> {
                 ),
                 activeIcon: Image.asset('assets/images/icon_basket_active.png'),
                 label: 'پرفروش ترین ها',
+              ),
+              // 3
+              BottomNavigationBarItem(
+                icon: Container(
+                  child: Image.asset('assets/images/icon_home.png'),
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: CustomColors.blueIndicator,
+                        blurRadius: 20.0,
+                        spreadRadius: -7.0,
+                        offset: Offset(0.0, 13.0),
+                      ),
+                    ],
+                  ),
+                ),
+                activeIcon: Image.asset('assets/images/icon_home_active.png'),
+                label: 'صفحه اصلی',
               ),
             ],
           ),
